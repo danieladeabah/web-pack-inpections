683d784c55994616b8705305d549ca137d92eec2
danieladeabah <daniel.adeabah@amalitech.com>
2025-07-23 22:58:08 +0000

    remove

--- commit-each-change.ps1
+++
@@ -1,6 +0,0 @@
-## This PowerShell script will commit each unstaged file one by one with the message 'inspect'
-$changes = git status --porcelain | Where-Object { ($_.Substring(0,2) -eq 'M ' -or $_.Substring(0,2) -eq 'A ' -or $_.Substring(0,2) -eq '??') } | ForEach-Object { $_.Substring(3) }
-foreach ($file in $changes) {
-    git add "$file"
-    git commit -m "inspect"
-}
