9e9d697a1d3203c65d9b035777079a95fd24dce9
danieladeabah <daniel.adeabah@amalitech.com>
2025-07-23 22:32:31 +0000

    inspect

--- commit-each-change.ps1
+++ commit-each-change.ps1
@@ -1,5 +1,5 @@
-# This PowerShell script will commit each changed file one by one with the message 'inspect'
-$changes = git status --porcelain | ForEach-Object { $_.Substring(3) }
+## This PowerShell script will commit each unstaged file one by one with the message 'inspect'
+$changes = git status --porcelain | Where-Object { ($_.Substring(0,2) -eq 'M ' -or $_.Substring(0,2) -eq 'A ' -or $_.Substring(0,2) -eq '??') } | ForEach-Object { $_.Substring(3) }
 foreach ($file in $changes) {
     git add "$file"
     git commit -m "inspect"
